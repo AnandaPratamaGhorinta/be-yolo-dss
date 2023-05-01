983682ba8bdec0988c439ef5c4f3aea0124233c0
AnandaPratamaGhorinta <ghorinta@gmail.com>
2023-05-01 20:54:52 +0700

    Add files via upload

---
+++ src/main/java/com/dss/fullstackdss/FullstackDssApplication.java
@@ -0,0 +1,13 @@
+package com.dss.fullstackdss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackDssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackDssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dss/fullstackdss/FullstackDssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.fullstackdss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackDssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
